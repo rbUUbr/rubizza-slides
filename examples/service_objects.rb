7dc77904b50f1fb6b2bfbd57928bd39993952c62
Kirill Tatchihin <kirabsuir@gmail.com>
2019-07-11 01:42:06 +0300

    Add service object example

---
+++ examples/service_objects.rb
@@ -0,0 +1,41 @@
+# service object without state holding
+class MakeGreet
+  def self.call
+    puts 'I made super greet!'
+  end
+end
+
+MakeGreet.call # => I made super greet!
+
+# service object that contains some logic
+class RecalculateArray
+  attr_accessor :numbers_array
+
+  def self.call(numbers_array)
+    new(numbers_array).call
+  end
+
+  def call
+    select_even_from_array
+    square
+    make_greet
+  end
+
+  def initialize(numbers_array)
+    @numbers_array = numbers_array
+  end
+
+  private
+
+  def select_even_from_array
+    numbers_array.select!(&:even?)
+  end
+
+  def square
+    numbers_array.map! { |number| number**2 }
+  end
+
+  def make_greet
+    MakeGreet.call
+  end
+end
---
+++ examples/wrapper.rb
@@ -0,0 +1,1 @@
+# It is your hometask, so feel free to open PR to fill in this file
